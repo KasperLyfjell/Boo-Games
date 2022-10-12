90ee8c92ce2cb299afa9644d883fd428470f4e4c
Bartosz <bartek.witek@onet.eu>
2022-10-12 12:04:20 +0200

    IntroCutscene
    Added sound to the intro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,14 +13,18 @@ public class GameManager : MonoBehaviour
 
     public SUPERCharacterAIO player;
 
+    public AudioSource Lighting;
+
     private void Start()
     {
         EditorLightUp.SetActive(false);
 
+#if UNITY_EDITOR
         if (PlayIntro)
             NewGameStart();
         else
             CutsceneEnd();
+#endif
 
 #if !UNITY_EDITOR //Build options
         NewGameStart();
@@ -33,6 +37,11 @@ public class GameManager : MonoBehaviour
     {
         Intro.Play();
     }
+    public void LightingStrike()
+    {
+        Lighting.Play();
+    }
+
     public void CutsceneEnd()
     {
         Debug.Log("you can walk now");
