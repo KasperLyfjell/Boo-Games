544756cad12b088d9b29b1615a42d01ad723eea0
Bartosz <bartek.witek@onet.eu>
2022-12-01 03:05:30 +0100

    Audio mixers & subtitles
    Changed the way subtitles are displayed to work with a single, long audio clip.
    
    Started working on audio mixers and snapshots to transition between effects on audio in the level (for example when entering flashback)

--- Assets/Scripts/Environment/FlashbackTrigger.cs
+++ Assets/Scripts/Environment/FlashbackTrigger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using SUPERCharacter;
+using UnityEngine.Audio;
 
 public class FlashbackTrigger : MonoBehaviour
 {
@@ -11,11 +12,19 @@ public class FlashbackTrigger : MonoBehaviour
 
     public bool CanTrigger;
 
+    [Header("Distortion VFX")]
     public GameObject ScreenDistortion;
     public TextMeshProUGUI SubtitleObj;
+    /*
+    //This can be a way to easily switch between audio effects
+    public AudioMixerGroup mixG;
+    public AudioMixer mix;
+    */
 
-    public List<AudioClip> Clips = new List<AudioClip>();
+    [Header("Subtitle Details")]
     public List<string> Subtitles = new List<string>();
+    [Tooltip("The total number of items in this list MUST be the same as the amount of items in 'Subtitle Details'")]
+    public List<float> subDelay = new List<float>();//Have this the same number as the total number of subtitles
 
     private SUPERCharacterAIO player;
 
@@ -30,34 +39,42 @@ public class FlashbackTrigger : MonoBehaviour
             //trigger the wibbly wobbly effect and transition to flashback
             //propably could be done with another coroutine to transition into flashback mode, which then it starts the sound
             player = other.gameObject.GetComponent<SUPERCharacterAIO>();
-            player.BeginFlashback();
-            StartCoroutine(PlayFlashback());
-            ScreenDistortion.SetActive(true);
+            StartCoroutine(InitiateFlashback());
             CanTrigger = false;
         }
     }
 
+    IEnumerator InitiateFlashback()
+    {
+
+
+        yield return new WaitForSeconds(2f);
+
+        player.BeginFlashback();
+        ScreenDistortion.SetActive(true);
+        StartCoroutine(PlayFlashback());
+    }
+
     IEnumerator PlayFlashback()
     {
-        AU.clip = Clips[playedAudios];
+        //Propably add some initiation first to fade into flashback
         AU.Play();
         SubtitleObj.text = Subtitles[playedAudios];
         SubtitleObj.gameObject.SetActive(true);
 
-        yield return new WaitForSeconds(AU.clip.length + 0.1f);
+        yield return new WaitForSeconds(subDelay[playedAudios]);
 
         SubtitleObj.gameObject.SetActive(false);
         playedAudios++;
 
-        if (playedAudios == Clips.Count)
+        if (playedAudios == Subtitles.Count)
             EndFlashback();
         else
-            StartCoroutine(PlayFlashback());//if that even works??
+            StartCoroutine(PlayFlashback());
     }
 
-    private void EndFlashback()
+    private void EndFlashback()        //stop the wibbly wobbly effect and go transition to normal
     {
-        //stop the wibbly wobbly effect and go transition to normal
         ScreenDistortion.SetActive(false);
         player.EndFlashback();
     }
