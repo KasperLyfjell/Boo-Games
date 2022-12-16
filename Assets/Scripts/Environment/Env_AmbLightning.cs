a0badb899e58625ff9c943d3e00086519891e117
Bella <isabella.sorensen@live.no>
2022-12-16 19:25:56 +0100

    Lightning light worked on
    Lightning is close to done, needs some connections to other scripts before it just needs to be turned on by telling a float to turn on

---
+++ Assets/Scripts/Environment/Env_AmbLightning.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class Env_AmbLightning : MonoBehaviour
+{
+    public HDAdditionalLightData lightningData;
+    public GameObject lightning;
+    public DynamicAudioZone dynAudio;
+    //public float audioZoneFloat; 
+
+    public float lightStrengthMax = 40000f;
+    public float lightStrengthMin = 0f;
+
+    public float lightningValue;
+    public float transitionSpeed = 0.2f;
+
+    public bool ActiveLightning;
+    void Start()
+    {
+        lightningValue = lightStrengthMax;
+
+    }
+
+    
+    void Update()
+    {
+        if (dynAudio.IsInside == false)
+        {
+
+            if(dynAudio.delay < 0.2) 
+            //Sjekk om thunder delay er mindre enn 1 og sett til true
+            //Spilles av pittelitt før lightning 
+            {
+                ActiveLightning = true;
+            }
+
+
+            if (ActiveLightning)
+            {
+                lightning.SetActive(true);
+                lightningValue = lightStrengthMax;
+                lightningData.intensity = lightningValue;
+
+
+                StartCoroutine(Wait());
+            }
+            else
+            {
+                lightning.SetActive(false);
+                lightningValue = lightStrengthMin;
+                lightningData.intensity = lightningValue;
+            }
+
+        }
+        
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(transitionSpeed);
+        ActiveLightning = false;
+    }
+}
---
+++ Assets/Scripts/Environment/Env_AmbLightning1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+public class Env_AmbLightning1 : MonoBehaviour
+{
+    public HDAdditionalLightData lightningData;
+    public GameObject lightning;
+
+    public float lightStrengthMax = 40000f;
+    public float lightStrengthMin = 0f;
+
+    public float lightningValue;
+    public float transitionSpeed = 0.2f;
+
+    public bool ActiveLightning;
+    void Start()
+    {
+        lightningValue = lightStrengthMax;
+    }
+
+    
+    void Update()
+    {
+        if (ActiveLightning)
+        {
+            lightning.SetActive(true);
+            lightningValue = lightStrengthMax;
+            lightningData.intensity = lightningValue;
+
+
+            StartCoroutine(Wait());
+        }
+        else
+        {
+            lightning.SetActive(false);
+            lightningValue = lightStrengthMin;
+            lightningData.intensity = lightningValue;
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(transitionSpeed);
+        ActiveLightning = false;
+    }
+}
