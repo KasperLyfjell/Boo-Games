c3b821480c476cdab40644d3ba3df2c80861c1e3
Bartosz <bartek.witek@onet.eu>
2022-12-18 22:37:03 +0100

    hallway trigger

--- Assets/Scripts/Environment/HallwayTrigger.cs
+++ Assets/Scripts/Environment/HallwayTrigger.cs
@@ -6,13 +6,18 @@ public class HallwayTrigger : MonoBehaviour
 {
     public GameObject player;
     public GameObject SpecialLight;
-    public GameObject Shadow;
+    public ShadowController Shadow;
 
+    public AudioSource playAudio;
 
     public MoveShadow ShadowTrigger;
 
     public Env_Door BedroomDoor;
     public Env_Door KidsDoor;
+
+    private bool triggered;
+    private Vector3 shadowStart;
+    private Vector3 shadowEnd;
 
 #if UNITY_EDITOR
     private void Update()
@@ -27,9 +32,9 @@ public class HallwayTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Player")
+        if(other.gameObject.name == "Player" && !triggered)
         {
-            BedroomDoor.MoveOpenDoor(60);
+            StartCoroutine(HallwayEvent());
         }
     }
 
@@ -38,7 +43,7 @@ public class HallwayTrigger : MonoBehaviour
         if (other.gameObject.name == "Player")
         {
             KidsDoor.ShutDoor();
-            Shadow.transform.position = new Vector3(1.39999998f, 75.0699997f, 447.230011f);
+            //Shadow.transform.position = new Vector3(1.39999998f, 75.0699997f, 447.230011f);
             //spawnShadow();
         }
     }
@@ -46,5 +51,21 @@ public class HallwayTrigger : MonoBehaviour
     public void spawnShadow()
     {
         ShadowTrigger.TriggerEvent();
+    }
+
+    IEnumerator HallwayEvent()
+    {
+        triggered = true;
+        playAudio.Play();
+        KidsDoor.ShutDoor();
+
+        yield return new WaitForSeconds(2);
+
+        SpecialLight.SetActive(true);
+        BedroomDoor.MoveOpenDoor(100);
+
+        yield return new WaitForSeconds(5);
+
+        Shadow.Emerge(shadowStart, shadowEnd);
     }
 }
